98095adf54855fdd034f1982215e8c5dff02d816
Aleksandyr <aleksandyr.kamenov@abv.bg>
2015-03-29 17:07:39 +0300

    some functionality on player

--- scripts/mian.js
+++ scripts/mian.js
@@ -1,2 +1,24 @@
 ﻿var canvas = Document.getElementById('canvas');
-var ctx = canvas.getContext('webgl');
+var ctx = canvas.getContext('2d');
+
+var input = new Input();
+attachListeners(input);
+
+var player = new player(100, 100);
+
+function update(){
+    this.tick();
+    this.render();
+    requestAnimationFrame(update);
+};
+
+function tick(){
+    player.update();
+}
+
+function render(ctx){
+    player.render(ctx);
+}
+
+update();
+
--- scripts/player.js
+++ scripts/player.js
@@ -1,1 +1,34 @@
-﻿
+﻿var Player = (function(){
+    function Player(x, y){
+        this.position = new Vector2(x, y);
+        this.movement = {left: false, right: false, up: false, down: false};
+        this.velocity = 2;
+
+        this.width = 173;
+        this.height = 271;
+
+        this.animation = new Animation(this.width, this.height, 1, 'ressources/images/F5S4.png', 1, 0, 0);
+        this.boundingBox = new Ranctangle(x, y, this.width, this.height);
+    }
+
+    Player.prototype.update = function(){
+        if(this.movement.right){
+            this.position.x += this.velocity;
+        } else if(this.movement.left){
+            this.position.x -= this.velocity;
+        }
+        if(this.movement.up){
+            this.position.y += this.velocity;
+        } else if(this.movement.down){
+            this.position.y -= this.velocity;
+        }
+
+        this.animation.update();
+    };
+
+    Player.prototype.render = function(ctx){
+        this.animation.render(ctx);
+    };
+
+    return Player;
+}());
