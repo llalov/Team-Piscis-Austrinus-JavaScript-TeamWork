03100658bf0b839027363debdabe3e7d0c55161b
Aleksandyr <aleksandyr.kamenov@abv.bg>
2015-03-31 23:41:44 +0300

    player limit

--- scripts/player.js
+++ scripts/player.js
@@ -1,4 +1,7 @@
-﻿var Player = (function(){
+﻿var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var Player = (function(){
     function Player(x, y) {
         this.position = new Vector2(x, y);
         this.movement = {left: false, right: false, up: false, down: false, fire: false};
@@ -11,6 +14,7 @@
 
         this.animation = new Animation(this.width, this.height, 0, 0, 1, 'ressources/images/F5S4.png', 1, 0, 0);
         this.boundingBox = new Rectangle(x, y, this.width, this.height);
+        //Background.prototype.canvasWidth = this.canvas.width;
 
     }
 
@@ -19,17 +23,33 @@
             this.animation = new Animation(this.width, this.height, 0, 0, 1,
                 'ressources/images/F5S4-right.png', 1, 0, 0);
             this.position.x += this.velocity;
+
+            if(this.position.x >= canvas.width - this.width){
+                this.position.x = canvas.width - this.width;
+            }
         } else if (this.movement.left) {
             this.animation = new Animation(this.width, this.height, 0, 0, 1,
                 'ressources/images/F5S4-left.png', 1, 0, 0);
             this.position.x -= this.velocity;
+
+            if(this.position.x <= 0){
+                this.position.x = 0;
+            }
         } else{
             this.animation = new Animation(this.width, this.height, 0, 0, 1,'ressources/images/F5S4.png', 1, 0, 0);
         }
         if (this.movement.up) {
             this.position.y -= this.velocity;
+
+            if(this.position.y <= canvas.height/4.5*3){
+                this.position.y = canvas.height/4.5*3;
+            }
         } else if (this.movement.down) {
             this.position.y += this.velocity;
+
+            if(this.y >= canvas.height - this.height){
+                this.position.y = canvas.height - this.height;
+            }
         }
 
         if(this.movement.fire){
