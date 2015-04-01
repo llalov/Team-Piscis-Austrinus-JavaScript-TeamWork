7018b747440a4c9b293ecc5b0ea47bbf57f2ec8a
Aleksandyr <aleksandyr.kamenov@abv.bg>
2015-04-01 17:24:45 +0300

    some menu

---
+++ scripts/menu.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function menu() {
+    var button = document.getElementById('StartButton');
+    var menu = document.getElementById('SplashScreen');
+    var canvas = document.getElementById('canvas');
+
+    menu.style.display = 'none';
+    canvas.style.display = 'block';
+};
